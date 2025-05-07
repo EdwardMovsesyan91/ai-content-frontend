61810b8e112dbcd19125bffa25f8e44249a367de
EdwardMovsesyan <edomovses@gmail.com>
2025-05-08 00:10:00 +0300

    dvh added in layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 
 export default function Layout() {
   return (
-    <div className="min-h-screen bg-background text-foreground">
+    <div className="min-h-dvh bg-background text-foreground">
       <Header />
       <main className="p-6">
         <Outlet />
